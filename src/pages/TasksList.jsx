d03a66208f3eb91ce59bb7435f48f0c5cb49f1b4
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-04-12 15:23:45 +0200

    milestone 12: create debounce function for the tasks filter

--- src/pages/TasksList.jsx
+++ src/pages/TasksList.jsx
@@ -1,34 +1,53 @@
-import { useContext, useMemo, useState } from "react";
+import { useCallback, useContext, useMemo, useState } from "react";
 import TaskRow from "../components/TaskRow";
 import { GlobalContext } from "../context/GlobalContext"
 
-export default function TasksList(){
+function debounce(callback, delay){
+  let timer;
+  return (value) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(value)
+    }, delay)
+  }
+}
+
+export default function TasksList() {
 
   const { tasks } = useContext(GlobalContext);
   console.log(tasks)
-  const [ sortBy, setSortBy ] = useState("createdAt");
-  const [ sortOrder, setSortOrder ] = useState(1);
+  const [sortBy, setSortBy] = useState("createdAt");
+  const [sortOrder, setSortOrder] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const sortedTasks = useMemo(() => {
+  const debounceSearch = useCallback(debounce(setSearchQuery, 500), []);
+
+  const sortedAndFilteredTasks = useMemo(() => {
 
     const sorted = [...tasks];
     sorted.sort((a, b) => {
-      if(sortBy === "title"){
+      if (sortBy === "title") {
         return a.title.localeCompare(b.title) * sortOrder;
-      } else if(sortBy === "status"){
+      } else if (sortBy === "status") {
         const statusOrder = { "To do": 1, "Doing": 2, "Done": 3 };
         return (statusOrder[a.status] - statusOrder[b.status]) * sortOrder;
-      } else if(sortBy === "createdAt"){
+      } else if (sortBy === "createdAt") {
         return (new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()) * sortOrder;
       }
       return 0;
     });
-    return sorted;
-  }, [tasks, sortBy, sortOrder]);
+    return sorted.filter(task => task.title.toLowerCase().includes(searchQuery.toLowerCase()));
+  }, [tasks, sortBy, sortOrder, searchQuery]);
 
 
-  return(
+  return (
     <>
+      <input
+        type="text"
+        onChange={event => debounceSearch(event.target.value)}
+        placeholder="Cerca task..."
+      />
+
       <h1>Tasks</h1>
       <table>
         <thead>
@@ -49,14 +68,14 @@ export default function TasksList(){
         </thead>
         <tbody>
           {
-            sortedTasks.map(task => (
-              <TaskRow key={task.id} task={task}/>
+            sortedAndFilteredTasks.map(task => (
+              <TaskRow key={task.id} task={task} />
             ))
-            
+
           }
         </tbody>
       </table>
-      
+
     </>
   )
 }
